94d3c6d65b6df2bdc313a7cd49f1b1366a186cc5
Jedri Visser <jedri@entersekt.com>
2019-02-22 12:08:52 +0200

    Remove outdated comment from get_cert_fingerprint.sh

--- scripts/get_cert_fingerprint.sh
+++ scripts/get_cert_fingerprint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Based on https://fidoalliance.org/specs/fido-v2.0-id-20180227/fido-appid-and-facets-v2.0-id-20180227.html#determining-the-facetid-of-a-calling-application
-# Can also be calculated from within app as shown here: https://fidoalliance.org/specs/fido-v2.0-id-20180227/fido-appid-and-facets-v2.0-id-20180227.html#obtaining-facetid-of-android-native-app
-
 echo Android Certificate fingerprint
 
 keytool -list -v -alias fido2 -storepass fido2password -keystore ../app/keystore.jks 2> /dev/null | grep SHA256:
